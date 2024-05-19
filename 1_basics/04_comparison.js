32f4514c14c30e133edc5ace5b503d202c072368
Riya Yadav <114867989+ry129@users.noreply.github.com>
2024-05-19 14:54:45 +0000

    comparison done

---
+++ 1_basics/04_comparison.js
@@ -0,0 +1,20 @@
+console.log(2>1);
+console.log(2>=1);
+console.log(2<1);
+console.log(2==1);
+console.log(2!=1);
+
+console.log("2">1);
+console.log("02">1);
+
+console,log(null>0);//mostly we avoid these typ pf conversion because they put us in confusion. we need clean code.
+console.log(null==0);
+console.log(null>=0);
+
+console.log(undefined==0)////mostly we avoid these typ pf conversion because they put us in confusion. we need clean code.
+console.log(undefined>0);
+console.log(undefined<0);
+
+//=== (strict check)
+
+console.log("2"===2);
