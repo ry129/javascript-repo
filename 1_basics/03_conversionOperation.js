d24ee60ab3de4ff748583feb89fc135b4a93710c
Riya Yadav <114867989+ry129@users.noreply.github.com>
2024-05-19 12:34:30 +0000

    operation done

--- 1_basics/03_conversionOperation.js
+++ 1_basics/03_conversionOperation.js
@@ -24,3 +24,34 @@ let booleanIsLoggedIn= Boolean(isLoggedIn)
  console.log(stringNumber);
 
  console.log(typeof stringNumber);
+
+ //***********************Operation***************************/
+
+ let value =3
+ let negValue=-value
+ console.log(negValue);
+
+ 
+ //console.log(2+2);
+ //console.log(2-2);
+ //console.log(2*2);
+ //console.log(2**3);
+ //console.log(2/3);
+ //console.log(2%3);
+
+ let str1="hello"
+ let str2="riya"
+ let str3=str1+str2
+
+//console.log(str3);
+
+//console.log("1"+2);
+//console.log(1+"2");
+//console.log("1"+2+2);
+//console.log(1+2+"2");
+console.log(+true);
+ 
+
+let gameCounter=100;
+gameCounter++;
+console.log(gameCounter);
