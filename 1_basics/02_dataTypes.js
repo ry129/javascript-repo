dc806d48eb0b9e184d25617a8e787d3e37dcddc6
Riya Yadav <114867989+ry129@users.noreply.github.com>
2024-05-19 11:22:11 +0000

    datatype

---
+++ 1_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict";//treat all JS code as newer version
+
+//alert(3+3)//we are using node js , not browser.
+console.log(3+3) ;//code readability should be high //
+console.log("riya")
+
+
+let name="riya"
+let age=18
+let isLoggedIn=false
+
+// number=>2 to power 53
+// bigint=if number is too big may be used for trading etc.
+
+// string=>""
+//boolean=> true/false
+//null => standalone value ; let state =null;
+//undefined => means let state;
+//symbol=used to find uniqueness
+
+
+//object
+
+console.log(typeof undefined); //type = undefined
+console.log(typeof null);//type=object
