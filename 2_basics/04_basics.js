f1ffd769b8cd133787beade8c46f43f59b5c9133
Riya Yadav <114867989+ry129@users.noreply.github.com>
2024-05-23 06:26:27 +0000

    JSON added

--- 2_basics/04_basics.js
+++ 2_basics/04_basics.js
@@ -54,4 +54,27 @@ users[1].email
 
 // console.log(tinderUser.hasOwnProperty('isLoggedIn'));
 
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
 
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+// {
+//     "name": "hitesh",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
