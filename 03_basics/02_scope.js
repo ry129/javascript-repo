0908e0343a12ec3d815bfe69fac28838f19610e5
Riya Yadav <114867989+ry129@users.noreply.github.com>
2024-05-24 14:37:23 +0000

    scope done

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -13,3 +13,45 @@ if (true) {
 // console.log(b);
 // console.log(c);
 
+function one(){
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+     two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
